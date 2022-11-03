8a81eac2184434cae3b2866df2ebf1a0cb109753
Idul <84653497+aidulcandra@users.noreply.github.com>
2022-11-03 19:33:21 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+import * as bot from './system/bot.js'
+import './system/server.js'
+
+/** @type {import('./system/bot.js').botEvents} */
+const events = {}
+
+
+
+
+// ===================================================================
+// ===================================================================
+
+// Ketika Bot terhubung dengan Whatsapp dan siap
+events.when_ready = async function () {
+  console.log("Bot Siap")
+}
+
+// Ketika Bot mendapat pesan (dari chat pribadi maupun grup)
+events.when_get_message = async function (message, sender, group) {
+  
+  // Skip pesan offline
+  if (message.isOffline) return
+
+  // Membalas (reply) pesan
+  if (message.text === "halo") {
+    await message.reply("Halo juga")
+  }
+
+  // Kirim pesan tanpa reply
+  if (message.text === "hai") {
+    await bot.sendText(message.room, "Hai juga")
+  }
+}
+
+//Untuk events lainnya, ketik "events." lalu tunggu autocomplete-nya
+
+// ===================================================================
+// ===================================================================
+
+
+bot.start(events)
